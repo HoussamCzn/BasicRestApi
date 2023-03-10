3b714915310b69e857708154a354927827ddef1c
Houssam <doriancoriace@gmail.com>
2023-03-10 09:20:06 +0100

    add basic routes

---
+++ app.js
@@ -0,0 +1,51 @@
+const express = require('express')
+
+const app = express()
+const port = 3000
+
+app.use((req, res, next) => {
+    console.log("[" + new Date().toISOString().replace(/T/, ' ').replace(/\..+/, '') + "]: " + req.url)
+    next()
+})
+
+app.get("/", (req, res) => {
+    res.send("Hello World!")
+})
+
+app.get("/welcome", (req, res) => {
+    res.send("Bienvenue sur le TP 1 du cours d'architecture logicielle")
+})
+
+app.get("/secret", (req, res) => {
+    res.status(401).send("Vous ne possédez pas les droits pour accéder à ma page secrète")
+})
+
+app.get("/error", (req, res) => {
+    res.status(500).send({
+        error: "Une erreur s'est produite"
+    })
+})
+
+app.get("/img", (req, res) => {
+    res.sendFile(__dirname + "/img.png")
+})
+
+app.get("/redirectMe", (req, res) => {
+    res.redirect("https://www.iut-littoral.fr/")
+})
+
+app.get("/users/:name", (req, res) => {
+    res.send("Bienvenue sur la page de " + req.params.name)
+})
+
+app.get("/somme", (req, res) => {
+    res.send("La somme de " + req.query.a + " et " + req.query.b + " est égale à " + (parseInt(req.query.a) + parseInt(req.query.b)))
+})
+
+app.use((req, res, next) => {
+    res.status(404).send("Cette page n'existe pas !")
+})
+
+app.listen(port, () => {
+    console.log("Example app listening on port ${port}")
+})
