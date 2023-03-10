f85d69df9a009d0db8ded792bd786a21ea635a06
Houssam <doriancoriace@gmail.com>
2023-03-10 11:38:00 +0100

    feat: discord bot can log into a server

--- TP3/src/app.js
+++ TP3/src/app.js
@@ -1,1 +1,26 @@
-// Votre fichier app.js
+const { Client, Events, GatewayIntentBits } = require('discord.js');
+const conf = require('../conf.json');
+const fs = require('node:fs');
+const path = require('node:path');
+const TOKEN = conf.token;
+
+const client = new Client({
+    intents: [GatewayIntentBits.Guilds]
+});
+
+client.login(TOKEN);
+
+const eventsPath = path.join(__dirname, 'events');
+const eventFiles = fs
+    .readdirSync(eventsPath)
+    .filter((file) => file.endsWith('.js'));
+
+for (const file of eventFiles) {
+    const filePath = path.join(eventsPath, file);
+    const event = require(filePath);
+    if (event.once) {
+        client.once(event.name, (...args) => event.execute(...args));
+    } else {
+        client.on(event.name, (...args) => event.execute(...args));
+    }
+}
