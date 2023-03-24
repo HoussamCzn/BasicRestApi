c356ae41e5a45fba1ae19e908fbc68b5f4b32b3d
Houssam <doriancoriace@gmail.com>
2023-03-24 10:55:57 +0100

    add main router to the app

--- TP2/src/app.js
+++ TP2/src/app.js
@@ -1,7 +1,11 @@
 const express = require('express')
 const { connectTodB } = require('./services/db/connection')
+const router = require('./routes/index')
+
 const app = express()
 const port = 3000
+
+app.use(router)
 
 connectTodB().then(() => {
     app.listen(port, () => {
