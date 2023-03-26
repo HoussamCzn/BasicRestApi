c8fd4ac427b7ee07c71443bd25b261f815c51ad8
Houssam <doriancoriace@gmail.com>
2023-03-26 21:29:36 +0200

    add subroutes to the main router
    Added the items and watchlists routes to the main router.

--- TP2/src/routes/index.js
+++ TP2/src/routes/index.js
@@ -1,8 +1,12 @@
 const { Router } = require('express');
 const usersRouter = require('./users.routes');
+const itemsRouter = require('./items.routes');
+const watchlistsRouter = require('./watchlists.routes');
 
 const mainRouter = Router();
 
 mainRouter.use('/users', usersRouter);
+mainRouter.use('/items', itemsRouter);
+mainRouter.use('/watchlists', watchlistsRouter);
 
 module.exports = mainRouter;
