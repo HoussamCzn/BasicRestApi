a0f3b60439e070c590b6db90634e67d24813dbd3
Houssam <doriancoriace@gmail.com>
2023-03-26 21:23:37 +0200

    add helper function to help building the query
    Added a new function that is allowed to edit the query to try to match the schemas. It will do by itself the necessary conversions if needed and will be called before the schema validation middleware.

--- TP2/src/services/schemaValidator.js
+++ TP2/src/services/schemaValidator.js
@@ -1,9 +1,39 @@
 const validate = require("jsonschema").validate;
 const _ = require("lodash");
 
+function _matchSchema(schema, item) {
+    for (const [name, property] of Object.entries(schema.properties)) {
+        if (property.type === "array") {
+            const arr = [];
+            item[name] = item[name].split(",").map((item) => item.trim());
+            if (property.items.type === "object") {
+                const size = Object.keys(property.items.properties).length;
+                for (let i = 0; i < item[name].length; i += size) {
+                    arr.push(
+                        {
+                            item_id: item[name][i],
+                            status: item[name][i + 1],
+                        }
+                    );
+                }
+                item[name] = arr;
+            } else {
+                item[name].forEach((item) => {
+                    if (property.items.type === "number") {
+                        item = Number(item);
+                    }
+                });
+            }
+        } else if (property.type === "number") {
+            item[name] = Number(item[name]);
+        }
+    }
+}
+
 function validateSchemaMiddleware(schema) {
     return (req, res, next) => {
         item = req.query;
+        _matchSchema(schema, item);
         const validation = validate(item, schema);
         if (_.isEmpty(validation.errors)) {
             return next();
