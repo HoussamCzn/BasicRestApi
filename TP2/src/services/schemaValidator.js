7759c6efc22ef51ecb44743973a13654240a677f
Houssam <doriancoriace@gmail.com>
2023-03-26 23:46:31 +0200

    fix: add missing if branch to a function
    Added missing branch in case the value type was "object" in _matchSchema helper function.

--- TP2/src/services/schemaValidator.js
+++ TP2/src/services/schemaValidator.js
@@ -13,6 +13,8 @@ function _matchSchema(schema, item) {
                     item[name] = item[name].map((item) => Number(item));
                 }
             }
+        } else if (property.type === "object") {
+            item[name] = JSON.parse(item[name]);
         } else if (property.type === "number") {
             item[name] = Number(item[name]);
         }
