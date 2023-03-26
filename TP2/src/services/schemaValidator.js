3dbd68e4b0e485eaeb17a63ad665d3291ca769e3
Houssam <doriancoriace@gmail.com>
2023-03-26 23:32:53 +0200

    replace comma separated query by JSON
    Replaced the self written part in the _matchSchema function taking a comma separated query by a call to JSON parse function to use a JSON formatted query rather than building myself objects.

--- TP2/src/services/schemaValidator.js
+++ TP2/src/services/schemaValidator.js
@@ -4,25 +4,14 @@ const _ = require("lodash");
 function _matchSchema(schema, item) {
     for (const [name, property] of Object.entries(schema.properties)) {
         if (property.type === "array") {
-            const arr = [];
-            item[name] = item[name].split(",").map((item) => item.trim());
             if (property.items.type === "object") {
-                const size = Object.keys(property.items.properties).length;
-                for (let i = 0; i < item[name].length; i += size) {
-                    arr.push(
-                        {
-                            item_id: item[name][i],
-                            status: item[name][i + 1],
-                        }
-                    );
-                }
-                item[name] = arr;
+                item[name] = JSON.parse(item[name]);
             } else {
-                item[name].forEach((item) => {
-                    if (property.items.type === "number") {
-                        item = Number(item);
-                    }
-                });
+                item[name] = item[name].split(",");
+                item[name] = item[name].map((item) => item.trim());
+                if (property.items.type === "number") {
+                    item[name] = item[name].map((item) => Number(item));
+                }
             }
         } else if (property.type === "number") {
             item[name] = Number(item[name]);
