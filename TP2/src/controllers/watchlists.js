82a94a66f114446195c73e02be56bcdf83b27310
Houssam <doriancoriace@gmail.com>
2023-03-31 14:15:18 +0200

    add a few functions to interact with watchlists
    Added functions to add an item to the watchlist, get content of a watchlist, edit the status of an item inside a watchlist and get all the watchlists of a specific owner.

--- TP2/src/controllers/watchlists.js
+++ TP2/src/controllers/watchlists.js
@@ -1,4 +1,4 @@
-const { find, insertOne } = require('../services/db/crud');
+const { find, insertOne, findOne, updateOne } = require('../services/db/crud');
 
 const collection = "watchlists"
 
@@ -15,6 +15,90 @@ const createWatchlist = async (req, res) => {
     }
 }
 
+const addItemToWatchlist = async (req, res) => {
+    try {
+        const item = JSON.parse(req.query.item);
+        const result = await updateOne(collection,
+            {
+                name: req.query.name
+            },
+            {
+                $push: {
+                    items: item
+                }
+            }
+        )
+        return res.status(200).json(result);
+    } catch (err) {
+        return res.status(500).json(
+            {
+                error: err.message,
+            }
+        );
+    }
+}
+
+const editItemStatus = async (req, res) => {
+    try {
+        const result = await updateOne(collection,
+            {
+                name: req.query.name,
+                "items.item_id": req.query.item_id
+            },
+            {
+                $set: {
+                    "items.$.status": req.query.status
+                }
+            }
+        )
+        return res.status(200).json(result);
+    } catch (err) {
+        return res.status(500).json(
+            {
+                error: err.message,
+            }
+        );
+    }
+}
+
+const getWatchlistContent = async (req, res) => {
+    try {
+        const watchlist = await findOne(collection,
+            {
+                name: req.query.name
+            }
+        )
+        return res.status(200).json(watchlist.items);
+    } catch (err) {
+        return res.status(500).json(
+            {
+                error: err.message,
+            }
+        );
+    }
+}
+
+const getUserWatchlists = async (req, res) => {
+    try {
+        const result = await find(collection,
+            {
+                owner: req.query.owner
+            }
+        )
+        return res.status(200).json(result);
+    } catch (err) {
+        return res.status(500).json(
+            {
+                error: err.message,
+            }
+        );
+    }
+}
+
 module.exports = {
-    createWatchlist
+    createWatchlist,
+    addItemToWatchlist,
+    getWatchlistContent,
+    editItemStatus,
+    getUserWatchlists
 }
--- TP2/src/routes/watchlists.routes.js
+++ TP2/src/routes/watchlists.routes.js
@@ -2,10 +2,16 @@ const express = require('express');
 const { getUUID } = require('../services/uuidProvider');
 const watchlistsSchema = require('../schemas/watchlist.schema')
 const { validateSchemaMiddleware } = require('../services/schemaValidator');
-const { createWatchlist } = require('../controllers/watchlists');
+const {
+    createWatchlist, addItemToWatchlist, getWatchlistContent, editItemStatus, getUserWatchlists
+} = require('../controllers/watchlists');
 
 const router = express.Router();
 
 router.post("/create", getUUID, validateSchemaMiddleware(watchlistsSchema), createWatchlist);
+router.post("/push", addItemToWatchlist);
+router.get("/content", getWatchlistContent);
+router.post("/edit", editItemStatus);
+router.get("/user/lists", getUserWatchlists);
 
 module.exports = router;
