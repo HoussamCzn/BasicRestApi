aa2a0acbddcb786227731b266c77adef83a95e77
Houssam <doriancoriace@gmail.com>
2023-04-01 17:42:45 +0200

    fix: improve the items filter

--- TP2/src/controllers/items.js
+++ TP2/src/controllers/items.js
@@ -17,7 +17,37 @@ const createItem = async (req, res) => {
 
 const getItems = async (req, res) => {
     try {
-        const result = await find(collection, req.query);
+        let result;
+        if (Object.keys(req.query).length === 0) {
+            result = await find(collection);
+        } else {
+            const item_title = req.query.title ? req.query.title : "";
+            const item_genre = req.query.genre ? req.query.genre : "";
+            const item_language = req.query.language ? req.query.language : "";
+            const item_duration = req.query.duration ? req.query.duration : "";
+            const item_imdbId = req.query.imdbId ? req.query.imdbId : "";
+            const item_id = req.query.id ? req.query.id : "";
+            result = await find(collection, {
+                title: {
+                    $regex: item_title,
+                },
+                genre: {
+                    $regex: item_genre,
+                },
+                language: {
+                    $regex: item_language,
+                },
+                duration: {
+                    $regex: item_duration,
+                },
+                imdbId: {
+                    $regex: item_imdbId,
+                },
+                id: {
+                    $regex: item_id,
+                }
+            });
+        }
         return res.status(200).json(result);
     } catch (err) {
         return res.status(500).json(
