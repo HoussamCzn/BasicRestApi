c4332d4697c7949759f75edc37770b0228ac0ab5
Houssam <doriancoriace@gmail.com>
2023-04-07 15:19:38 +0200

    feat: change last name and first name writings
    Forced last name to be upper case and first name first letter to be upper case.

--- TP2/src/controllers/users.js
+++ TP2/src/controllers/users.js
@@ -4,6 +4,8 @@ const collection = "users"
 
 const createUser = async (req, res) => {
     try {
+        req.query.lastName = req.query.lastName.toUpperCase();
+        req.query.firstName = req.query.firstName.charAt(0).toUpperCase() + req.query.firstName.slice(1);
         const result = await insertOne(collection, req.query);
         return res.status(200).json(result);
     } catch (err) {
